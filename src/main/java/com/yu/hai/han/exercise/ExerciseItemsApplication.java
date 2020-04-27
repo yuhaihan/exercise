f50781e2c3bcb8ded128ca52c0b7fb11a59951b8
yuhaihan <1448590372@qq.com>
2020-04-27 13:47:11 +0800

    first commit

---
+++ src/main/java/com/yu/hai/han/exercise/ExerciseItemsApplication.java
@@ -0,0 +1,13 @@
+package com.yu.hai.han.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yu/hai/han/exercise/ExerciseItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.hai.han.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
